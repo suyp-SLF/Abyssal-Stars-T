f3ebcb00fcf2ccf5996040de7c52969174442f5f
suyp <1299019344@qq.com>
2025-07-23 05:21:30 +0800

    fix one healthbar change all change

--- ComponentSystem/component_healthy.gd
+++ ComponentSystem/component_healthy.gd
@@ -13,7 +13,11 @@ func take_damage(amount: float) -> void:
 	
 func display_health() -> void:
 	var cal: float = current_healthy/max_healthy
-	$Actual/health.material.set_shader_parameter("health", cal)
+	$Actual/healthnum.text = str(current_healthy)
+	
+	var unique_mat = create_unique_material($Actual/health.material)
+	unique_mat.set_shader_parameter("health", cal)
+	$Actual/health.material = unique_mat
 	
 func heal(amount: int) -> void:
 	current_healthy = min(current_healthy + amount, max_healthy)
@@ -22,3 +26,9 @@ func _ready():
 	area_2d
 	max_healthy = 100
 	current_healthy = 100
+	display_health()
+
+func create_unique_material(base_material):
+	var new_mat = base_material.duplicate()
+	new_mat.shader = base_material.shader.duplicate()
+	return new_mat
